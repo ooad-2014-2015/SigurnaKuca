4d34c308499c3119f6ab0c220606b9e17962c0fb
Senka Ibrahimpašić <sibrahimpa1@etf.unsa.ba>
2015-05-09 01:13:05 +0200

    Projekat

--- RPR/SafeHouse/Forme/DoktorForm.Designer.cs
+++ RPR/SafeHouse/Forme/DoktorForm.Designer.cs
@@ -28,76 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBoxImeDok = new System.Windows.Forms.TextBox();
-            this.textBoxPrezimeDok = new System.Windows.Forms.TextBox();
-            this.textBoxOpisDok = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox_listaPacijenata = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_ispisDatumaSistematskog = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ime:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Prezime:";
+            this.pictureBox1.Location = new System.Drawing.Point(11, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 134);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 100);
+            this.label3.Location = new System.Drawing.Point(14, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Opis:";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ime doktora";
             // 
-            // textBoxImeDok
+            // label4
             // 
-            this.textBoxImeDok.Location = new System.Drawing.Point(120, 28);
-            this.textBoxImeDok.Name = "textBoxImeDok";
-            this.textBoxImeDok.Size = new System.Drawing.Size(125, 20);
-            this.textBoxImeDok.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Prezime doktora";
             // 
-            // textBoxPrezimeDok
+            // button1
             // 
-            this.textBoxPrezimeDok.Location = new System.Drawing.Point(120, 64);
-            this.textBoxPrezimeDok.Name = "textBoxPrezimeDok";
-            this.textBoxPrezimeDok.Size = new System.Drawing.Size(125, 20);
-            this.textBoxPrezimeDok.TabIndex = 4;
+            this.button1.Location = new System.Drawing.Point(12, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Pogledaj raspored";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBoxOpisDok
+            // listBox_listaPacijenata
             // 
-            this.textBoxOpisDok.Location = new System.Drawing.Point(120, 97);
-            this.textBoxOpisDok.Name = "textBoxOpisDok";
-            this.textBoxOpisDok.Size = new System.Drawing.Size(125, 20);
-            this.textBoxOpisDok.TabIndex = 5;
+            this.listBox_listaPacijenata.FormattingEnabled = true;
+            this.listBox_listaPacijenata.Location = new System.Drawing.Point(186, 56);
+            this.listBox_listaPacijenata.Name = "listBox_listaPacijenata";
+            this.listBox_listaPacijenata.Size = new System.Drawing.Size(161, 303);
+            this.listBox_listaPacijenata.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Lista pacijenata: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label_ispisDatumaSistematskog);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.richTextBox3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(364, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 350);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Podaci o korisniku";
+            // 
+            // label_ispisDatumaSistematskog
+            // 
+            this.label_ispisDatumaSistematskog.AutoSize = true;
+            this.label_ispisDatumaSistematskog.Location = new System.Drawing.Point(499, 330);
+            this.label_ispisDatumaSistematskog.Name = "label_ispisDatumaSistematskog";
+            this.label_ispisDatumaSistematskog.Size = new System.Drawing.Size(71, 13);
+            this.label_ispisDatumaSistematskog.TabIndex = 7;
+            this.label_ispisDatumaSistematskog.Text = "(ispis datuma)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(304, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Datum zadnjeg sistematskog pregleda:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(307, 47);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(266, 165);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 21);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Historija nalaza";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(9, 235);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(269, 108);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Lične bilješke";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ažurirajte nalaze podacima za zadnjeg sistematskog: ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(9, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(266, 108);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(314, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Historija nalaza";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 24);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Registruj promjene";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // DoktorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 262);
-            this.Controls.Add(this.textBoxOpisDok);
-            this.Controls.Add(this.textBoxPrezimeDok);
-            this.Controls.Add(this.textBoxImeDok);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(945, 370);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_listaPacijenata);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "DoktorForm";
             this.Text = "Doktor";
-            this.Load += new System.EventHandler(this.DoktorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,11 +231,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBoxImeDok;
-        private System.Windows.Forms.TextBox textBoxPrezimeDok;
-        private System.Windows.Forms.TextBox textBoxOpisDok;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox_listaPacijenata;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label_ispisDatumaSistematskog;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        
     }
 }
