1df6de630cd907cb6cc3010cf237b289fde44057
Haris Musovic <hmusovic1@etf.unsa.ba>
2015-06-03 20:31:04 +0200

    Projekat

--- WpfApplication2/WpfApplication2/Igrica.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'c:\users\nejra\documents\visual studio 2012\Projects\WpfApplication2\WpfApplication2\Igrica.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
